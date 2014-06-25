43b36d73d45c5f4bdff641118074ebc9148cd908
Garth <garth0323@gmail.com>
2014-06-25 08:18:11 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vidtuts::Application.config.secret_key_base = 'a088ef40509c9a10885dfa3fd5d90446ede46275d087e67b0e045f6ebd46287059c44d73fff288e4a21c043fd3621ff012d3a9dc82aebac6db3567d470166638'
